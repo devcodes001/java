221f2a13ee7980058b378f79f074148211f52b57
dev <devnarayanpunthala3@gmail.com>
2026-03-16 15:23:08 +0530

    Update Averagecalculator.java

--- Averagecalculator.java
+++ Averagecalculator.java
@@ -1,33 +1,42 @@
+import java.util.Scanner;
+
 class NegativeNumberException extends Exception {
     public NegativeNumberException() {
         super("Negative numbers are not allowed.");
     }
 }
 
-public class AverageCalculator {
+public class Averagecalculator {
 
     public static double calculateAverage(int[] numbers) throws NegativeNumberException {
         if (numbers.length == 0) return 0;
-
         int sum = 0;
         for (int num : numbers) {
-            if (num < 0) {
-                throw new NegativeNumberException();
-            }
+            if (num < 0) throw new NegativeNumberException();
             sum += num;
         }
-
         return (double) sum / numbers.length;
     }
 
     public static void main(String[] args) {
-        int[] data = {5, 10, -2, 8};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers do you want to enter? ");
+        int count = scanner.nextInt();
+        int[] data = new int[count];
+
+        System.out.println("Enter " + count + " numbers:");
+        for (int i = 0; i < count; i++) {
+            data[i] = scanner.nextInt();
+        }
 
         try {
             double avg = calculateAverage(data);
             System.out.println("Average: " + avg);
         } catch (NegativeNumberException e) {
-            System.err.println(e.getMessage());
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
         }
     }
 }
